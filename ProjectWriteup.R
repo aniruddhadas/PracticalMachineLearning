0a0f6a4e611737430ed9815ee2c46b512dcd8f19
and <NORTHAMERICA\and>
2014-08-24 11:43:59 -0400

    project solutions.

--- ProjectWriteup.R
+++ ProjectWriteup.R
@@ -26,5 +26,13 @@ varImp(random.forest)
 testing <- read.csv2("pml-testing.csv",sep = ",", na.strings = c("", "NA", "#DIV/0!") ) 
 validTestingFeatures <- colnames(testing[colSums(is.na(testing)) == 0])[-(1:7)][-53]
 testingTuned <- testing[validTestingFeatures]
-for(i in c(2:ncol(testingTuned))) {testingTuned[,i] = as.numeric(as.character(testingTuned[,i]))}
-predictionsTest <- predict(random.forest, newdata=validTrainDataTrain)
+for(i in c(1:ncol(testingTuned))) {testingTuned[,i] = as.numeric(as.character(testingTuned[,i]))}
+predictionsTest <- predict(random.forest, newdata=testingTuned)
+predictionsTest
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
