125652bafe295a3499facf9c2a5aec2e2183b791
and <NORTHAMERICA\and>
2014-08-24 11:35:23 -0400

    few fixes

--- ProjectWriteup.R
+++ ProjectWriteup.R
@@ -1,47 +1,30 @@
 getwd()
-setwd("C:/and/PracticalMachineLearning")
+setwd("C:/Users/and/Documents/GitHub/PracticalMachineLearning")
 dir()
-#read the data
-data <- read.csv2("pml-training.csv",sep = ",", na.strings = c("", "NA", "#DIV/0!") ) 
-data <- read.csv2("pml-training.csv",sep = ",") 
-head(data)
-finaltesting <- read.csv2("pml-testing.csv",sep = ",", na.strings = c("", "NA", "#DIV/0!") ) 
-# ignore name, timestamp etc. columns
-validFeatures <- colnames(data[colSums(is.na(data)) == 0])[-(1:7)]
-validTrainingData <- data[validFeatures]
-#validTrainingData$classe <- as.factor(validTrainingData$classe)
-#we will not be touching the test data
-inTrain <- createDataPartition(y=validTrainingData$classe, p=0.7, list=FALSE)
-validTrainingData <- validTrainingData[inTrain,]
-validTestingData <- validTrainingData[-inTrain,]
-
+train <- read.csv2("pml-training.csv",sep = ",", na.strings = c("", "NA", "#DIV/0!") ) 
+head(train)
+validTrainFeatures <- colnames(train[colSums(is.na(train)) == 0])[-(1:7)]
+validTrainData <- train[validTrainFeatures]
+head(validTrainData)
 complete <- function(x) {x[,sapply(x, function(y) !any(is.na(y)))] }
-validTrainingData <- complete(validTrainingData)
-for(i in c(2:ncol(validTrainingData)-1)) {validTrainingData[,i] = as.numeric(as.character(validTrainingData[,i]))}
-
-validTestingData <- complete(validTestingData)
-for(i in c(2:ncol(validTestingData)-1)) {validTestingData[,i] = as.numeric(as.character(validTestingData[,i]))}
-
-
-
-#modFit <- train(classe~.,method="rf",data=validTrainingData,importance=TRUE)
-cData <- validTrainingData
-for(i in c(2:ncol(cData)-1)) {cData[,i] = as.numeric(as.character(cData[,i]))}
-
+validTrainData <- complete(validTrainData)
+for(i in c(2:ncol(validTrainData)-1)) {validTrainData[,i] = as.numeric(as.character(validTrainData[,i]))}
+str(validTrainData)
+library(caret)
+inTrain <- createDataPartition(y=validTrainData$classe, p=0.7, list=FALSE)
+validTrainDataTrain <- validTrainData[inTrain,]
+validTrainDataTest <- validTrainData[-inTrain,]
 library(randomForest)
-random.forest <- train(cData[,-57],
-                       cData$classe,
+random.forest <- train(validTrainDataTrain[,-53],
+                       validTrainDataTrain$classe,
                        tuneGrid=data.frame(mtry=3),
                        trControl=trainControl(method="none")
-                    )
+)
 
-
-predictionsTr <- predict(random.forest, newdata=cData)
-confusionMatrix(predictionsTr,cData$classe)
-
-predictionsTe <- predict(random.forest, newdata=validTestingData)
-confusionMatrix(predictionsTe,validTestingData$classe)
-
-predictionsTe <- predict(modFit, newdata=validTestingData)
-confusionMatrix(predictionsTe,validTestingData$classe)
-
+varImp(random.forest)
+#now do the same with the final testing dataset variables
+testing <- read.csv2("pml-testing.csv",sep = ",", na.strings = c("", "NA", "#DIV/0!") ) 
+validTestingFeatures <- colnames(testing[colSums(is.na(testing)) == 0])[-(1:7)][-53]
+testingTuned <- testing[validTestingFeatures]
+for(i in c(2:ncol(testingTuned))) {testingTuned[,i] = as.numeric(as.character(testingTuned[,i]))}
+predictionsTest <- predict(random.forest, newdata=validTrainDataTrain)
